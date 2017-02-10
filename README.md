59e2b60b907f970f2d22325a06a0decbbad1d98e
Victor Zhen <vz97@Victors-MacBook-Pro.local>
2017-02-09 19:41:27 -0500

    editted some things

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-# astardjikstra
-A comparison of shortest path algorithms.
-=======
-# ConvexHullAlgos
->>>>>>> 12b396b6139ad399c4d3a31d48c6258e830cf521
+# Convex Hull Algos
+
+asdf
--- js/buttons.js
+++ js/buttons.js
@@ -3,31 +3,57 @@ $(document).ready(function(){
   $('.bottomTexts').hide();
   $('.matrix-container').hide();
   $('.table-container-references').hide();
+  $('#TitleText1').hide();
+    $('#TitleText2').hide();
+    $('#TitleText3').hide();
   $("#appButton").click(function(){
     $('.bottomTexts').hide();
     $('.matrix-container').hide();
     $('.table-container-references').hide();
+    $('#JarvisMarchText').hide();
+    $('#GrahamScanText').hide();
+    $('#ChansAlgoText').hide();
     $('#firstText').slideToggle();
+    $('#TitleText1').hide();
+    $('#TitleText2').hide();
+    $('#TitleText3').hide();
   });
   $("#algButton").click(function(){
     $('.bottomTexts').hide();
     $('.matrix-container').hide();
     $('.table-container-references').hide();
-    $('#secondText').slideToggle();
+    $('#JarvisMarchText').slideToggle();
+    $('#GrahamScanText').slideToggle();
+    $('#ChansAlgoText').slideToggle();
     $('.matrix-container').slideToggle();
     $('#tempText').slideToggle();
+    $('#TitleText1').slideToggle();
+    $('#TitleText2').slideToggle();
+    $('#TitleText3').slideToggle();
   });
   $("#inspButton").click(function(){
     $('.bottomTexts').hide();
     $('.matrix-container').hide();
     $('.table-container-references').hide();
+    $('#JarvisMarchText').hide();
+    $('#GrahamScanText').hide();
+    $('#ChansAlgoText').hide();
     $('#thirdText').slideToggle();
+    $('#TitleText1').hide();
+    $('#TitleText2').hide();
+    $('#TitleText3').hide();
   });
   $("#refButton").click(function(){
     $('.bottomTexts').hide();
     $('.matrix-container').hide();
     $('.table-container-references').hide();
+    $('#JarvisMarchText').hide();
+    $('#GrahamScanText').hide();
+    $('#ChansAlgoText').hide();
     $('#fourthText').slideToggle();
     $('.table-container-references').slideToggle();
+    $('#TitleText1').hide();
+    $('#TitleText2').hide();
+    $('#TitleText3').hide();
   });
 });
